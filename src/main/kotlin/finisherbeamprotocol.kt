34057dc2c775a6921fc4a4e73e8d9c707f85c780
Genir <mateusz.mikolaj.wojcik@gmail.com>
2023-11-04 22:37:17 +0100

    finisher beam protocol description

--- src/main/kotlin/finisherbeamprotocol.kt
+++ src/main/kotlin/finisherbeamprotocol.kt
@@ -4,6 +4,8 @@ import com.fs.starfarer.api.combat.BaseHullMod
 import com.fs.starfarer.api.combat.DamageType
 import com.fs.starfarer.api.combat.ShipAPI
 import com.fs.starfarer.api.combat.WeaponAPI
+import com.fs.starfarer.api.combat.ShipAPI.HullSize
+
 
 class FinisherBeamProtocol : BaseHullMod() {
     override fun applyEffectsAfterShipCreation(ship: ShipAPI?, id: String?) {
@@ -14,6 +16,14 @@ class FinisherBeamProtocol : BaseHullMod() {
             .filter { isFinisherBeam(it) }
             .forEach { setFinisherBeamProtocol(it) }
     }
+
+    override fun getDescriptionParam(index: Int, hullSize: HullSize?): String? = when (index) {
+        0 -> "Tachyon Lance"
+        1 -> "Phase Lance"
+        2 -> "High Intensity Laser"
+        3 -> "autofire"
+        else -> null
+    }
 }
 
 fun setFinisherBeamProtocol(weapon: WeaponAPI) {
@@ -22,7 +32,7 @@ fun setFinisherBeamProtocol(weapon: WeaponAPI) {
     weapon.spec.aiHints.addAll(
         listOf(
             WeaponAPI.AIHints.USE_LESS_VS_SHIELDS,
-            // STRIKE tag prevents wasting shots at fighters.
+            // STRIKE tag prevents wasting shots on fighters.
             WeaponAPI.AIHints.STRIKE,
             // USE_VS_FRIGATES is needed because STRIKE forbids attacking frigates.
             WeaponAPI.AIHints.USE_VS_FRIGATES,
