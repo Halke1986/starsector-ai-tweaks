b25516e0dc81bcd193343759dbb8fc2fa4123524
Genir <mateusz.mikolaj.wojcik@gmail.com>
2023-11-16 21:29:47 +0100

    debug plugin

---
+++ src/main/kotlin/DebugPlugin.kt
@@ -0,0 +1,53 @@
+package com.genir.aitweaks
+
+import com.fs.starfarer.api.GameState
+import com.fs.starfarer.api.Global
+import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin
+import com.fs.starfarer.api.combat.CombatEngineAPI
+import com.fs.starfarer.api.combat.ViewportAPI
+import com.fs.starfarer.api.input.InputEventAPI
+import org.lazywizard.lazylib.ui.LazyFont
+import java.awt.Color
+
+const val debug = false
+var debugStr = "test"
+
+// DebugPlugin is used to render debug information during combat.
+// It's disabled by default.
+class DebugPlugin : BaseEveryFrameCombatPlugin() {
+    private var engine: CombatEngineAPI? = null
+    private var font: LazyFont? = null
+    private var drawable: LazyFont.DrawableString? = null
+    private var oldDebugStr = ""
+
+    override fun advance(amount: Float, events: MutableList<InputEventAPI>?) {
+        super.advance(amount, events)
+        if (!debug || Global.getCurrentState() != GameState.COMBAT)
+            return
+
+        if (engine == null) {
+            this.engine = Global.getCombatEngine()
+            this.font = LazyFont.loadFont("graphics/fonts/insignia15LTaa.fnt")
+        }
+
+        debug()
+    }
+
+    override fun renderInUICoords(viewport: ViewportAPI?) {
+        super.renderInUICoords(viewport)
+        if (!debug || font == null)
+            return
+
+        if (debugStr != oldDebugStr) {
+            oldDebugStr = debugStr
+            drawable = font!!.createText(debugStr, baseColor = Color.ORANGE)
+        }
+
+        drawable?.draw(500f, 500f)
+    }
+
+    private fun debug() {
+    }
+}
+
+
