01d889cca2d1a1f27e97860515848ff664f661c1
Genir <mateusz.mikolaj.wojcik@gmail.com>
2023-12-12 01:14:45 +0100

    initial focus fire

--- src/main/kotlin/features/FocusOnTarget.kt
+++ src/main/kotlin/features/FocusOnTarget.kt
@@ -7,11 +7,15 @@ import com.genir.aitweaks.maneuverTarget
 import org.lwjgl.util.vector.Vector2f
 
 fun applyFocusOnTargetAI(ship: ShipAPI) {
-    ship.weaponGroupsCopy.forEach { group ->
-        val plugins = group.aiPlugins
-        for (i in plugins.indices) {
-            if (plugins[i].weapon.slot.isHardpoint && plugins[i].weapon.firesForward()) {
-                plugins[i] = FocusOnTargetAI(plugins[i])
+    val groups = ship.weaponGroupsCopy
+    for (i in groups.indices) {
+        val plugins = groups[i].aiPlugins
+        for (j in plugins.indices) {
+            val weapon = plugins[j].weapon
+            if ((weapon.slot.isHardpoint && weapon.firesForward())
+                || (i == 1 && weapon.ship.owner == 0)
+            ) {
+                plugins[j] = FocusOnTargetAI(plugins[j])
             }
         }
     }
