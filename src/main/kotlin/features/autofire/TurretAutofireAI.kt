c7de45a78147e3e84a822700c194b8a10f84502a
Genir <mateusz.mikolaj.wojcik@gmail.com>
2023-12-25 18:27:39 +0100

    accuracy

--- src/main/kotlin/features/autofire/TurretAutofireAI.kt
+++ src/main/kotlin/features/autofire/TurretAutofireAI.kt
@@ -1,36 +1,47 @@
 package com.genir.aitweaks.features.autofire
 
 import com.fs.starfarer.api.combat.AutofireAIPlugin
+import com.fs.starfarer.api.combat.CombatEntityAPI
 import com.fs.starfarer.api.combat.MissileAPI
 import com.fs.starfarer.api.combat.ShipAPI
 import com.fs.starfarer.api.combat.WeaponAPI
 import com.genir.aitweaks.debugValue
+import com.genir.aitweaks.utils.div
+import com.genir.aitweaks.utils.extensions.hasBestTargetLeading
 import com.genir.aitweaks.utils.extensions.isValidTarget
 import com.genir.aitweaks.utils.extensions.maneuverTarget
+import org.lazywizard.lazylib.ext.minus
+import org.lazywizard.lazylib.ext.plus
 import org.lwjgl.util.vector.Vector2f
 
 // TODO
-// accuracy
+// hardpoint
+
+// small pd weapons by hullmod
+// ignore flares
+// only beams attack phased ships
+
 // fire on shields
 // ff
 // target selection
 // paladin ff
 // ir lance tracks fighters
-// small pd weapons by hullmod
 // track ship target for player
-// ignore flares
-// only beams attack phased ships
 // dont switch targets mid burst
 
 class TurretAutofireAI(private val weapon: WeaponAPI) : AutofireAIPlugin {
-    private val maneuverTargetTracker = ManeuverTargetTracker(weapon.ship)
     private var solution: FiringSolution? = null
+    private var maneuverTarget: ShipAPI? = null
+    private var prevTarget: CombatEntityAPI? = null
 
+    private var attackTime: Float = 0f
+    private var idleTime: Float = 0f
 
     override fun advance(timeDelta: Float) {
-        maneuverTargetTracker.advance(timeDelta)
+        trackManeuverTarget(timeDelta)
+        trackTimes(timeDelta)
 
-        solution = selectTarget(weapon, solution?.target, maneuverTargetTracker.target)
+        solution = selectTarget(weapon, solution?.target, maneuverTarget)
     }
 
     override fun shouldFire(): Boolean {
@@ -61,20 +72,48 @@ class TurretAutofireAI(private val weapon: WeaponAPI) : AutofireAIPlugin {
         solution = null
     }
 
-    override fun getTarget(): Vector2f? = solution?.intercept
+    override fun getTarget(): Vector2f? {
+        if (solution?.valid != true) return null
+
+        val offset = solution!!.intercept - solution!!.target.location
+        return solution!!.target.location + offset / getAccuracy()
+    }
+
     override fun getTargetShip(): ShipAPI? = solution?.target as? ShipAPI
     override fun getWeapon(): WeaponAPI = weapon
     override fun getTargetMissile(): MissileAPI? = solution?.target as? MissileAPI
-}
 
-class ManeuverTargetTracker(private val ship: ShipAPI) {
-    var target: ShipAPI? = null
+    private fun trackManeuverTarget(timeDelta: Float) {
+        val newTarget = weapon.ship.maneuverTarget
+        if (newTarget != null || maneuverTarget?.isValidTarget != true) maneuverTarget = newTarget
+    }
 
-    fun advance(timeDelta: Float) {
-        when (val newTarget = ship.maneuverTarget) {
-            target -> return
-            null -> if (target?.isValidTarget != true) target = null
-            else -> target = newTarget
+    private fun trackTimes(timeDelta: Float) {
+        val currentTarget = solution?.target
+        if (currentTarget != null && prevTarget != currentTarget) {
+            prevTarget = currentTarget
+            attackTime = 0f
         }
+
+        if (weapon.isFiring) {
+            attackTime += timeDelta
+            idleTime = 0f
+        } else idleTime += timeDelta
+
+        if (idleTime >= 3f) attackTime = 0f
+    }
+
+    /**
+     * getAccuracy returns current weapon accuracy.
+     * The value is a number in range [1.0;2.0]. 1.0 is perfect accuracy, 2.0f is the worst accuracy.
+     * For worst accuracy, the weapon should aim exactly in the middle point between the target actual
+     * position and calculated intercept position.
+     */
+    private fun getAccuracy(): Float {
+        if (weapon.hasBestTargetLeading) return 1f
+
+        val accBase = weapon.ship.aimAccuracy
+        val accBonus = weapon.spec.autofireAccBonus
+        return (accBase - (accBonus + attackTime / 15f)).coerceAtLeast(1f)
     }
 }
