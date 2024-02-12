817c1bd414bbcb791b5310599f1b54adb404cd2e
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-02-12 22:00:04 +0100

    hold fire enum

--- src/com/genir/aitweaks/features/autofire/AttackRules.kt
+++ src/com/genir/aitweaks/features/autofire/AttackRules.kt
@@ -6,57 +6,60 @@ import com.fs.starfarer.api.combat.WeaponAPI
 import com.genir.aitweaks.features.autofire.extensions.*
 import com.genir.aitweaks.utils.shieldUptime
 
-const val holdFire = false
-const val fire = true
+val fire = null
 
-fun avoidPhased(weapon: WeaponAPI, hit: Hit): Boolean = when {
+enum class HoldFire {
+    AVOID_PHASED, AVOID_SHIELDS, AVOID_EXPOSED_HULL, AVOID_MISSING_HULL, AVOID_FF, NO_TARGET, STABILIZE_ON_TARGET, NO_HIT_EXPECTED, OUT_OF_RANGE, FORCE_OFF,
+}
+
+fun avoidPhased(weapon: WeaponAPI, hit: Hit): HoldFire? = when {
     (hit.target as? ShipAPI)?.isPhased != true -> fire
 
-    weapon.conserveAmmo -> holdFire
+    weapon.conserveAmmo -> HoldFire.AVOID_PHASED
     weapon.isPD -> fire
     weapon.isBeam -> fire
 
-    else -> holdFire
+    else -> HoldFire.AVOID_PHASED
 }
 
-fun avoidWrongDamageType(weapon: WeaponAPI, hit: Hit, params: Params): Boolean = when {
+fun avoidWrongDamageType(weapon: WeaponAPI, hit: Hit, params: Params): HoldFire? = when {
     !hit.target.isShip -> fire
     weapon.isStrictlyAntiShield -> avoidExposedHull(weapon, hit)
-    weapon.hasAIHint(WeaponAPI.AIHints.USE_LESS_VS_SHIELDS) -> avoidShields(weapon, hit) && aimAtHull(
+    weapon.hasAIHint(WeaponAPI.AIHints.USE_LESS_VS_SHIELDS) -> avoidShields(weapon, hit) ?: aimAtHull(
         weapon, hit, params
     )
 
     weapon.damageType == DamageType.HIGH_EXPLOSIVE -> aimAtHull(weapon, hit, params)
-
     else -> fire
 }
 
 /** Ensure projectile will not hit shields. */
-fun avoidShields(weapon: WeaponAPI, hit: Hit): Boolean = when {
+fun avoidShields(weapon: WeaponAPI, hit: Hit): HoldFire? = when {
     !hit.target.isShip -> fire
-    hit.shieldHit && weapon.conserveAmmo -> holdFire // weapons strict about saving ammo
-    hit.shieldHit && shieldUptime(hit.target.shield) > 0.8f -> holdFire // attack when shields flicker
+    hit.shieldHit && weapon.conserveAmmo -> HoldFire.AVOID_SHIELDS
+    hit.shieldHit && shieldUptime(hit.target.shield) > 0.8f -> HoldFire.AVOID_SHIELDS
     else -> fire
 }
 
 /** Ensure projectile will not hit exposed hull. */
-fun avoidExposedHull(weapon: WeaponAPI, hit: Hit): Boolean = when {
+fun avoidExposedHull(weapon: WeaponAPI, hit: Hit): HoldFire? = when {
     !hit.target.isShip -> fire
-    !hit.shieldHit || shieldUptime(hit.target.shield) < 0.8f -> holdFire // avoid shield flicker
+    !hit.shieldHit -> HoldFire.AVOID_EXPOSED_HULL
+    shieldUptime(hit.target.shield) < 0.8f -> HoldFire.AVOID_EXPOSED_HULL // avoid shield flicker
     else -> fire
 }
 
 /** Ensure projectile will hit hull. Shields are ignored. */
-fun aimAtHull(weapon: WeaponAPI, hit: Hit, params: Params): Boolean = when {
+fun aimAtHull(weapon: WeaponAPI, hit: Hit, params: Params): HoldFire? = when {
     !hit.target.isShip -> fire
     !hit.shieldHit -> fire // hit on hull was already predicted
     willHitBounds(
         weapon, hit.target as ShipAPI, params
-    ).let { it == null || it > weapon.totalRange } -> holdFire // ensure hull is in range, underneath the shields
+    ).let { it == null || it > weapon.totalRange } -> HoldFire.AVOID_MISSING_HULL // ensure hull is in range, underneath the shields
     else -> fire
 }
 
-fun avoidFriendlyFire(weapon: WeaponAPI, expected: Hit, actual: Hit?): Boolean {
+fun avoidFriendlyFire(weapon: WeaponAPI, expected: Hit, actual: Hit?): HoldFire? {
     if (actual == null || actual.target !is ShipAPI) return fire
 
     val target = expected.target
@@ -69,6 +72,6 @@ fun avoidFriendlyFire(weapon: WeaponAPI, expected: Hit, actual: Hit?): Boolean {
     if (!firePassesTarget && !blockerAheadOfTarget) return fire
 
     val friendly = actual.target.owner == weapon.ship.owner
-    return if (friendly || (actual.target.isInert && blockerAheadOfTarget)) holdFire
+    return if (friendly || (actual.target.isInert && blockerAheadOfTarget)) HoldFire.AVOID_FF
     else fire
 }
