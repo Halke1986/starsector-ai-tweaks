74703862a59125d49473e953a9dff398606469ac
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-02-13 20:21:32 +0100

    use isExpired method

--- src/com/genir/aitweaks/features/autofire/FindTarget.kt
+++ src/com/genir/aitweaks/features/autofire/FindTarget.kt
@@ -20,12 +20,6 @@ fun selectTarget(
     return selectShip(weapon, current, shipTarget, params)
 }
 
-fun selectMissile(w: WeaponAPI, current: CombatEntityAPI?, params: Params): CombatEntityAPI? =
-    selectEntity<MissileAPI>(w, current, params, missileGrid()) { it.isValidTarget && (!it.isFlare || w.ignoresFlares) }
-
-fun selectFighter(weapon: WeaponAPI, current: CombatEntityAPI?, params: Params): CombatEntityAPI? =
-    selectEntity<ShipAPI>(weapon, current, params, shipGrid()) { it.isAlive && it.isFighter }
-
 /** Shoot asteroids only when the weapon and asteroid are both in viewport.
  * Otherwise, it looks weird on the title screen. */
 fun selectAsteroid(weapon: WeaponAPI, current: CombatEntityAPI?, params: Params): CombatEntityAPI? {
@@ -34,21 +28,26 @@ fun selectAsteroid(weapon: WeaponAPI, current: CombatEntityAPI?, params: Params)
     return selectEntity<CombatAsteroidAPI>(weapon, current, params, asteroidGrid()) { inViewport(it.location) }
 }
 
+fun selectMissile(w: WeaponAPI, current: CombatEntityAPI?, params: Params): CombatEntityAPI? =
+    selectEntity<MissileAPI>(w, current, params, missileGrid()) { !it.isFlare || !w.ignoresFlares }
+
+fun selectFighter(weapon: WeaponAPI, current: CombatEntityAPI?, params: Params): CombatEntityAPI? =
+    selectEntity<ShipAPI>(weapon, current, params, shipGrid()) { it.isFighter }
+
 fun selectShip(weapon: WeaponAPI, current: CombatEntityAPI?, shipTarget: ShipAPI?, params: Params): CombatEntityAPI? {
     // Prioritize ship target. Non-PD hardpoint weapons track only ships target.
-    if (shipTarget?.isAlive == true && ((weapon.slot.isHardpoint && !weapon.isPD) || canTrack(
+    if (shipTarget?.isValidTarget == true && ((weapon.slot.isHardpoint && !weapon.isPD) || canTrack(
             weapon, Target(shipTarget), params
         ))
     ) return shipTarget
 
-    return selectEntity<ShipAPI>(
-        weapon, current, params, shipGrid()
-    ) { !it.isInert && (!it.isFighter || weapon.hasAIHint(ANTI_FTR)) }
+    return selectEntity<ShipAPI>(weapon, current, params, shipGrid()) { !it.isFighter || weapon.hasAIHint(ANTI_FTR) }
 }
 
 fun firstShipAlongLineOfFire(weapon: WeaponAPI, target: CombatEntityAPI, params: Params): Hit? =
-    closestEntityFinder<ShipAPI>(weapon, shipGrid()) {
+    closestEntityFinder(weapon, shipGrid()) {
         when {
+            it !is ShipAPI -> null
             it == target -> null
             it == weapon.ship -> null
             it.isFighter -> null
@@ -60,72 +59,35 @@ fun firstShipAlongLineOfFire(weapon: WeaponAPI, target: CombatEntityAPI, params:
         }
     }
 
-data class Hit(val target: CombatEntityAPI, val range: Float, val shieldHit: Boolean) {
-    override fun toString(): String {
-        val name = when {
-            target is MissileAPI -> "missile"
-            (target as ShipAPI).isHulk -> "hulk"
-            target.name == null -> target.hullSpec.hullId
-            else -> target.name
-        }
-
-        return "($name $range $shieldHit)"
-    }
-}
-
-/** Analyzes the potential collision between projectile and target. Null if no collision. */
-fun analyzeHit(weapon: WeaponAPI, target: CombatEntityAPI, params: Params): Hit? {
-    val targetCircumference = if (hasShield(target)) targetShield(target as ShipAPI) else Target(target)
-    val range = willHitCircumference(weapon, targetCircumference, params) ?: return null
-
-    // Simple circumference collision is enough for missiles and fighters.
-    if (!target.isShip) return Hit(target, range, false)
-
-    // Check shield hit.
-    if (hasShield(target)) willHitShield(weapon, target as ShipAPI, params)?.let { return Hit(target, it, true) }
-
-    // Check bounds hit.
-    return willHitBounds(weapon, target as ShipAPI, params)?.let { return Hit(target, it, false) }
-}
-
-fun analyzeAllyHit(weapon: WeaponAPI, ally: ShipAPI, params: Params): Hit? = when {
-    weapon.projectileCollisionClass == CollisionClass.PROJECTILE_FIGHTER -> null
-    weapon.projectileCollisionClass == CollisionClass.RAY_FIGHTER -> null
-    !willHitShieldCautious(weapon, ally, params) -> null
-    else -> Hit(ally, closestHitRange(weapon, targetShield(ally), params)!!, false)
-}
-
-/** Workaround for hulks retaining outdated ShieldAPI */
-fun hasShield(target: CombatEntityAPI): Boolean = target.isShip && !(target as ShipAPI).isHulk
-
 private inline fun <reified T : CombatEntityAPI> selectEntity(
     weapon: WeaponAPI,
     current: CombatEntityAPI?,
     params: Params,
     grid: CollisionGridAPI,
-    crossinline isValidTarget: (T) -> Boolean
+    crossinline isAcceptableTarget: (T) -> Boolean
 ): CombatEntityAPI? {
     // Try tracking current target.
-    if (current is T && isValidTarget(current) && canTrack(weapon, Target(current), params)) return current
+    if (current is T && isAcceptableTarget(current) && canTrack(weapon, Target(current), params)) return current
 
     // Find the closest enemy entity that can be tracked by the weapon.
-    return closestEntityFinder<T>(weapon, grid) {
+    return closestEntityFinder(weapon, grid) {
         when {
+            it !is T -> null
             it.owner == weapon.ship.owner -> null
-            !isValidTarget(it) -> null
+            !it.isValidTarget -> null
+            !isAcceptableTarget(it) -> null
             !canTrack(weapon, Target(it), params) -> null
             else -> Hit(it, closestHitRange(weapon, Target(it), params)!!, false)
         }
     }?.target
 }
 
-@Suppress("UNCHECKED_CAST")
-private fun <T : CombatEntityAPI> closestEntityFinder(weapon: WeaponAPI, grid: CollisionGridAPI, f: (T) -> Hit?): Hit? {
+private fun closestEntityFinder(weapon: WeaponAPI, grid: CollisionGridAPI, f: (CombatEntityAPI) -> Hit?): Hit? {
     var closestRange = weapon.totalRange
     var closestHit: Hit? = null
 
     val forEachFn = fun(entity: CombatEntityAPI) {
-        val hit = f(entity as T) ?: return
+        val hit = f(entity) ?: return
         if (hit.range < closestRange) {
             closestRange = hit.range
             closestHit = hit
--- src/com/genir/aitweaks/features/autofire/extensions/CombatEntityAPI.kt
+++ src/com/genir/aitweaks/features/autofire/extensions/CombatEntityAPI.kt
@@ -6,3 +6,6 @@ import com.fs.starfarer.api.combat.ShipAPI
 /** Is the entity a true ship, not a missile or fighter. */
 val CombatEntityAPI.isShip: Boolean
     get() = (this is ShipAPI) && !this.isFighter
+
+val CombatEntityAPI.isValidTarget: Boolean
+    get() = !this.isExpired && (this as? ShipAPI)?.isAlive != false
