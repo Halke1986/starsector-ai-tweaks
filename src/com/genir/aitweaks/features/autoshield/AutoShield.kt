0786cc9b7f7475147104d3fd4f698c8a56942ad2
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-02-16 20:30:08 +0100

    auto shield attempt 2

--- src/com/genir/aitweaks/features/autoshield/AutoShield.kt
+++ src/com/genir/aitweaks/features/autoshield/AutoShield.kt
@@ -1,11 +1,8 @@
 package com.genir.aitweaks.features.autoshield
 
 import com.fs.starfarer.api.Global
-import com.fs.starfarer.api.combat.BaseCombatLayeredRenderingPlugin
-import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin
-import com.fs.starfarer.api.combat.CombatEngineLayers
+import com.fs.starfarer.api.combat.*
 import com.fs.starfarer.api.combat.ShieldAPI.ShieldType
-import com.fs.starfarer.api.combat.ViewportAPI
 import com.fs.starfarer.api.input.InputEventAPI
 import com.fs.starfarer.api.util.Misc
 import com.genir.aitweaks.debugPlugin
@@ -17,35 +14,80 @@ import java.util.*
 const val ID = "com.genir.aitweaks.autoshield.AutoShield"
 
 class AutoShield : BaseEveryFrameCombatPlugin() {
-    private var isShieldOn = false
+    private var wantShieldOnForAutoOmni = false
+    private var doNotUseShields = false
+
+    private var prevPlayerShip: ShipAPI? = null
+
+//    private var isAIActive = false
+//    private var initialized = false
 
     override fun advance(amount: Float, events: MutableList<InputEventAPI>?) {
-        // Register render plugin.
         val engine = Global.getCombatEngine() ?: return
+
+        debugPlugin[0] = StateAccess.getAutoOmni()
+        debugPlugin[1] = wantShieldOnForAutoOmni
+
+        // Register render plugin.
         if (!engine.customData.containsKey(ID)) {
             engine.addLayeredRenderingPlugin(Renderer())
             engine.customData[ID] = true
         }
 
-        if (!engine.isUIAutopilotOn) return
+        // Player ship has changed.
+        if (engine.playerShip != prevPlayerShip) {
+            prevPlayerShip = engine.playerShip
+            wantShieldOnForAutoOmni = false
+            doNotUseShields = false
+        }
+
+        // The ship is on autopilot.
+        if (!engine.isUIAutopilotOn) {
+            wantShieldOnForAutoOmni = false
+        }
+
+        // Do work only for ship with omni shield.
         val shield = engine.playerShip?.shield ?: return
         if (shield.type != ShieldType.OMNI) return
 
-        debugPlugin[0] = StateAccess.getAutoOmni()
-        debugPlugin[1] = isShieldOn
-
-        when {
-            shield.isOn -> isShieldOn = true
-            !StateAccess.getAutoOmni() -> isShieldOn = shield.isOn
-        }
-
+        // Handle input
         events?.forEach {
             when {
                 it.isConsumed -> Unit
-                it.isRMBDownEvent -> isShieldOn = !isShieldOn
                 it.isMouseDownEvent && it.eventValue == 2 -> StateAccess.setAutoOmni(!StateAccess.getAutoOmni())
+                it.isRMBDownEvent && StateAccess.getAutoOmni() -> {
+                    doNotUseShields = shield.isOn
+                    wantShieldOnForAutoOmni = !shield.isOn
+                }
             }
         }
+
+
+//        // Initialize the AI state.
+//        if (!initialized) {
+//            StateAccess.setAutoOmni(true) // TODO tmp
+//            initialized = true
+//            isAIActive = true
+//        }
+//
+//        // Ship is on autopilot.
+//        if (!engine.isUIAutopilotOn) {
+//            isAIActive = false
+//            return
+//        }
+
+//        isAIActive = isAIActive and StateAccess.getAutoOmni()
+
+
+//        debugPlugin[0] = StateAccess.getAutoOmni()
+//        debugPlugin[1] = isAIActive
+
+//        when {
+//            shield.isOn -> isAIActive = true
+//            !StateAccess.getAutoOmni() -> isAIActive = shield.isOn
+//        }
+
+
     }
 
     inner class Renderer() : BaseCombatLayeredRenderingPlugin() {
@@ -54,14 +96,8 @@ class AutoShield : BaseEveryFrameCombatPlugin() {
             val ship = engine.playerShip ?: return
             val shield = ship.shield ?: return
 
-
-
-            when {
-                !StateAccess.getAutoOmni() -> return
-                !isShieldOn -> return
-                !engine.isUIAutopilotOn -> return
-                shield.type != ShieldType.OMNI -> return
-            }
+//            if (!StateAccess.getAutoOmni() || !wantShieldOnForAutoOmni) return
+            if (!StateAccess.getAutoOmni() || doNotUseShields) return
 
             glPushAttrib(GL_ALL_ATTRIB_BITS)
 
@@ -69,22 +105,17 @@ class AutoShield : BaseEveryFrameCombatPlugin() {
             glEnable(GL_BLEND)
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-            Misc.setColor(Color.GREEN)
+            Misc.setColor(Color.BLUE)
             glLineWidth(2f / Global.getCombatEngine().viewport.viewMult)
 
-            drawCircle(ship.location.x, ship.location.y, shield.radius, 64, false)
+            drawCircle(shield.location.x, shield.location.y, shield.radius, 64, false)
 
             glPopAttrib()
         }
 
 
-        override fun getRenderRadius(): Float {
-            return 999999f
-        }
+        override fun getRenderRadius(): Float = 1e6f
 
-        override fun getActiveLayers(): EnumSet<CombatEngineLayers> {
-            return EnumSet.of(CombatEngineLayers.JUST_BELOW_WIDGETS)
-        }
+        override fun getActiveLayers(): EnumSet<CombatEngineLayers> = EnumSet.of(CombatEngineLayers.JUST_BELOW_WIDGETS)
     }
-
 }
