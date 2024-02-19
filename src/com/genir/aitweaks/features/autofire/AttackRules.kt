267780d1e16e2ab51318102cba77d2b2aa3e78b4
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-02-20 00:57:43 +0100

    conserve ammo

--- src/com/genir/aitweaks/features/autofire/AttackRules.kt
+++ src/com/genir/aitweaks/features/autofire/AttackRules.kt
@@ -3,6 +3,8 @@ package com.genir.aitweaks.features.autofire
 import com.fs.starfarer.api.combat.DamageType
 import com.fs.starfarer.api.combat.ShipAPI
 import com.fs.starfarer.api.combat.WeaponAPI
+import com.fs.starfarer.api.combat.WeaponAPI.AIHints.ANTI_FTR
+import com.fs.starfarer.api.combat.WeaponAPI.AIHints.USE_LESS_VS_SHIELDS
 import com.genir.aitweaks.features.autofire.extensions.*
 import com.genir.aitweaks.utils.firingCycle
 import com.genir.aitweaks.utils.shieldUptime
@@ -11,56 +13,65 @@ import kotlin.math.min
 val fire = null
 
 enum class HoldFire {
-    AVOID_PHASED, AVOID_SHIELDS, AVOID_EXPOSED_HULL, AVOID_MISSING_HULL, AVOID_FF, NO_TARGET, STABILIZE_ON_TARGET, NO_HIT_EXPECTED, OUT_OF_RANGE, FORCE_OFF,
+    AVOID_PHASED, AVOID_SHIELDS, AVOID_EXPOSED_HULL, AVOID_MISSING_HULL, AVOID_FF, NO_TARGET, STABILIZE_ON_TARGET, NO_HIT_EXPECTED, OUT_OF_RANGE, FORCE_OFF, CONSERVE_AMMO
 }
 
-fun avoidPhased(weapon: WeaponAPI, hit: Hit): HoldFire? = when {
-    (hit.target as? ShipAPI)?.isPhased != true -> fire
+class AttackRules(private val weapon: WeaponAPI, private val hit: Hit, private val params: Params) {
+    val shouldHoldFire = avoidPhased() ?: conserveAmmo() ?: avoidWrongDamageType()
 
-    weapon.conserveAmmo -> HoldFire.AVOID_PHASED
-    weapon.isPD -> fire
-    weapon.isBeam -> fire
+    private fun avoidPhased(): HoldFire? = when {
+        (hit.target as? ShipAPI)?.isPhased != true -> fire
 
-    else -> HoldFire.AVOID_PHASED
-}
+        weapon.conserveAmmo -> HoldFire.AVOID_PHASED
+        weapon.isPD -> fire
+        weapon.isBeam -> fire
 
-fun avoidWrongDamageType(weapon: WeaponAPI, hit: Hit, params: Params): HoldFire? = when {
-    !hit.target.isShip -> fire
-    weapon.isStrictlyAntiShield -> avoidExposedHull(weapon, hit)
-    weapon.hasAIHint(WeaponAPI.AIHints.USE_LESS_VS_SHIELDS) -> avoidShields(weapon, hit) ?: aimAtHull(
-        weapon, hit, params
-    )
+        else -> HoldFire.AVOID_PHASED
+    }
 
-    weapon.damageType == DamageType.HIGH_EXPLOSIVE -> aimAtHull(weapon, hit, params)
-    else -> fire
-}
+    private fun conserveAmmo(): HoldFire? = when {
+        weapon.isPD != hit.target.isShip -> fire
+        weapon.hasAIHint(ANTI_FTR) && (hit.target as? ShipAPI)?.isFighter == true -> fire
+        !weapon.usesAmmo() -> fire
+        weapon.ammoTracker.maxAmmo - weapon.ammoTracker.ammo >= weapon.ammoTracker.reloadSize -> HoldFire.CONSERVE_AMMO
+        else -> fire
+    }
 
-/** Ensure projectile will not hit shields. */
-fun avoidShields(weapon: WeaponAPI, hit: Hit): HoldFire? = when {
-    !hit.target.isShip -> fire
-    hit.shieldHit && weapon.conserveAmmo -> HoldFire.AVOID_SHIELDS
-    hit.shieldHit && shieldUptime(hit.target.shield) > 0.8f -> HoldFire.AVOID_SHIELDS
-    else -> fire
-}
+    private fun avoidWrongDamageType(): HoldFire? = when {
+        !hit.target.isShip -> fire
+        weapon.isStrictlyAntiShield -> avoidExposedHull()
+        weapon.hasAIHint(USE_LESS_VS_SHIELDS) -> avoidShields() ?: aimAtHull()
+        weapon.damageType == DamageType.HIGH_EXPLOSIVE -> aimAtHull()
+        else -> fire
+    }
 
-/** Ensure projectile will not hit exposed hull. */
-fun avoidExposedHull(weapon: WeaponAPI, hit: Hit): HoldFire? = when {
-    !hit.target.isShip -> fire
-    !hit.shieldHit -> HoldFire.AVOID_EXPOSED_HULL
-    shieldUptime(hit.target.shield) < min(
-        0.8f, firingCycle(weapon).duration
-    ) -> HoldFire.AVOID_EXPOSED_HULL // avoid shield flicker
-    else -> fire
-}
+    /** Ensure projectile will not hit shields. */
+    private fun avoidShields(): HoldFire? = when {
+        !hit.target.isShip -> fire
+        hit.shieldHit && weapon.conserveAmmo -> HoldFire.AVOID_SHIELDS
+        hit.shieldHit && shieldUptime(hit.target.shield) > 0.8f -> HoldFire.AVOID_SHIELDS
+        else -> fire
+    }
 
-/** Ensure projectile will hit hull. Shields are ignored. */
-fun aimAtHull(weapon: WeaponAPI, hit: Hit, params: Params): HoldFire? = when {
-    !hit.target.isShip -> fire
-    !hit.shieldHit -> fire // hit on hull was already predicted
-    willHitBounds(
-        weapon, hit.target as ShipAPI, params
-    ).let { it == null || it > weapon.totalRange } -> HoldFire.AVOID_MISSING_HULL // ensure hull is in range, underneath the shields
-    else -> fire
+    /** Ensure projectile will not hit exposed hull. */
+    private fun avoidExposedHull(): HoldFire? = when {
+        !hit.target.isShip -> fire
+        !hit.shieldHit -> HoldFire.AVOID_EXPOSED_HULL
+        shieldUptime(hit.target.shield) < min(
+            0.8f, firingCycle(weapon).duration
+        ) -> HoldFire.AVOID_EXPOSED_HULL // avoid shield flicker
+        else -> fire
+    }
+
+    /** Ensure projectile will hit hull. Shields are ignored. */
+    private fun aimAtHull(): HoldFire? = when {
+        !hit.target.isShip -> fire
+        !hit.shieldHit -> fire // hit on hull was already predicted
+        willHitBounds(
+            weapon, hit.target as ShipAPI, params
+        ).let { it == null || it > weapon.totalRange } -> HoldFire.AVOID_MISSING_HULL // ensure hull is in range, underneath the shields
+        else -> fire
+    }
 }
 
 fun avoidFriendlyFire(weapon: WeaponAPI, expected: Hit, actual: Hit?): HoldFire? {
