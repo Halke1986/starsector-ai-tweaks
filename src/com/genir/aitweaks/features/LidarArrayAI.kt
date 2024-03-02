d911702ada90e679a9cfc5da2e3ed0057f8bd6d0
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-03-02 21:54:44 +0100

    lidar array ai stub

---
+++ src/com/genir/aitweaks/features/LidarArrayAI.kt
@@ -0,0 +1,12 @@
+package com.genir.aitweaks.features
+
+import com.fs.starfarer.api.combat.*
+import org.lwjgl.util.vector.Vector2f
+
+class LidarArrayAI : ShipSystemAIScript {
+    override fun init(ship: ShipAPI?, system: ShipSystemAPI?, flags: ShipwideAIFlags?, engine: CombatEngineAPI?) {
+    }
+
+    override fun advance(amount: Float, missileDangerDir: Vector2f?, collisionDangerDir: Vector2f?, target: ShipAPI?) {
+    }
+}
