eb54b6734931cc416ded4476aa89fb5993ed64a3
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-04-05 20:10:58 +0200

    src_asm dir

--- assemble.sh
+++ assemble.sh
@@ -1,4 +1,6 @@
-find ./src/com/genir/aitweaks/asm -type f -exec sed -i 's^com/fs/starfarer/combat/ai/BasicShipAI^com/genir/aitweaks/asm/AssemblyShipAI^g' {} +
-find ./src/com/genir/aitweaks/asm -type f -exec sed -i 's^com/fs/starfarer/combat/ai/I^com/genir/aitweaks/asm/OrderResponseModule^g' {} +
+asm_root="./src_asm/com.genir.aitweaks.combat.ai"
 
-python ../../../Krakatau/assemble.py -out ./jars/AITweaks_asm.jar -r ./src/com/genir/aitweaks/asm
+find $asm_root -type f -exec sed -i 's^com/fs/starfarer/combat/ai/BasicShipAI^com/genir/aitweaks/asm/AssemblyShipAI^g' {} +
+find $asm_root -type f -exec sed -i 's^com/fs/starfarer/combat/ai/I^com/genir/aitweaks/asm/OrderResponseModule^g' {} +
+
+python ../../../Krakatau/assemble.py -out ./jars/AITweaks_asm.jar -r $asm_root
