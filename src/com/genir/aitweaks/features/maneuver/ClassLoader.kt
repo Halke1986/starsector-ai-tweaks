cab7b6f823bb19de46b4f574070d60a3eace7780
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-04-28 01:39:16 +0200

    ai transformed from vanilla source

--- src/com/genir/aitweaks/features/maneuver/ClassLoader.kt
+++ src/com/genir/aitweaks/features/maneuver/ClassLoader.kt
@@ -1,15 +1,35 @@
 package com.genir.aitweaks.features.maneuver
 
-import com.genir.aitweaks.features.maneuver.raw.*
+import com.genir.aitweaks.utils.CCT
+
+private const val vanillaPath = "com/fs/starfarer"
+private const val asmPath = "com/genir/aitweaks"
 
 class AIClassLoader : ClassLoader() {
-    private val raws: Map<String, String> = mapOf(
-        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI" to assemblyShipAI,
-        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI\$o" to `assemblyShipAI$o`,
-        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI\$1" to `assemblyShipAI$1`,
-        "com.genir.aitweaks.asm.combat.ai.OrderResponseModule" to orderResponseModule,
-        "com.genir.aitweaks.asm.combat.ai.OrderResponseModule\$o" to `orderResponseModule$o`,
+//    private val raws: Map<String, String> = mapOf(
+//        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI" to assemblyShipAI,
+//        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI\$o" to `assemblyShipAI$o`,
+//        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI\$1" to `assemblyShipAI$1`,
+//        "com.genir.aitweaks.asm.combat.ai.OrderResponseModule" to orderResponseModule,
+//        "com.genir.aitweaks.asm.combat.ai.OrderResponseModule\$o" to `orderResponseModule$o`,
+//    )
+
+    private val sources: Map<String, String> = mapOf(
+        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI" to "com.fs.starfarer.combat.ai.BasicShipAI",
+        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI\$o" to "com.fs.starfarer.combat.ai.BasicShipAI\$o",
+        "com.genir.aitweaks.asm.combat.ai.AssemblyShipAI\$1" to "com.fs.starfarer.combat.ai.BasicShipAI\$1",
+        "com.genir.aitweaks.asm.combat.ai.OrderResponseModule" to "com.fs.starfarer.combat.ai.I",
+        "com.genir.aitweaks.asm.combat.ai.OrderResponseModule\$o" to "com.fs.starfarer.combat.ai.I\$o",
     )
+
+    private val transformer = CCT(listOf(
+        CCT.newTransform("$vanillaPath/combat/ai/BasicShipAI", "$asmPath/asm/combat/ai/AssemblyShipAI"),
+        CCT.newTransform("$vanillaPath/combat/ai/I", "$asmPath/asm/combat/ai/OrderResponseModule"),
+        CCT.newTransform("$vanillaPath/combat/ai/movement/BasicEngineAI", "$asmPath/features/maneuver/OverrideEngineAI"),
+        CCT.newTransform("$vanillaPath/combat/ai/movement/maneuvers/StrafeTargetManeuverV2", "$asmPath/features/maneuver/Strafe"),
+        CCT.newTransform("$vanillaPath/combat/ai/movement/maneuvers/B", "$asmPath/features/maneuver/Approach"),
+        CCT.newTransform("$vanillaPath/combat/ai/movement/maneuvers/U", "$asmPath/features/maneuver/Move"),
+    ))
 
     private var cache: MutableMap<String, Class<*>> = mutableMapOf()
 
@@ -19,7 +39,7 @@ class AIClassLoader : ClassLoader() {
             cache.containsKey(name) -> cache[name]!!
 
             // Load class from raw bytes and store in cache.
-            raws.containsKey(name) -> {
+            sources.containsKey(name) -> {
                 val c = findClass(name)
                 cache[name] = c
                 c
@@ -31,7 +51,8 @@ class AIClassLoader : ClassLoader() {
     }
 
     override fun findClass(name: String): Class<*> {
-        val classData = raws[name]!!.decodeHex()
+        val vanillaClassData = CCT.readClassBuffer(this.javaClass.classLoader, sources[name]!!)
+        val classData = transformer.apply(vanillaClassData)
         return defineClass(name, classData, 0, classData.size);
     }
 }
--- test/maneuver/ClassLoaderTest.kt
+++ test/maneuver/ClassLoaderTest.kt
@@ -1,5 +1,9 @@
 package maneuver
 
+import com.genir.aitweaks.utils.CCT
+import com.genir.aitweaks.utils.ClassConstantTransformer
+import com.genir.aitweaks.utils.ClassConstantTransformer.Transform
+import com.genir.aitweaks.utils.ClassConstantTransformer.newTransform
 import org.junit.jupiter.api.Test
 
 class CustomClassLoader {
@@ -7,12 +11,75 @@ class CustomClassLoader {
     fun testLoadClass() {
         val cl = this.javaClass.classLoader
 
-        val basicShipAI = cl.loadClass("com.fs.starfarer.combat.ai.BasicShipAI")
-        val basicShipAIStream = cl.getResourceAsStream("com/fs/starfarer/combat/ai/BasicShipAI.class")
+        val vanillaPath = "com/fs/starfarer"
+        val asmPath = "com/genir/aitweaks"
+
+        val transformer1 = ClassConstantTransformer(listOf<Transform>(
+            newTransform("$vanillaPath/combat/ai/BasicShipAI", "$asmPath/asm/combat/ai/AssemblyShipAI"),
+            newTransform("$vanillaPath/combat/ai/I", "$asmPath/asm/combat/ai/OrderResponseModule"),
+
+            newTransform("$vanillaPath/combat/ai/movement/BasicEngineAI", "$asmPath/features/maneuver/OverrideEngineAI"),
+            newTransform("$vanillaPath/combat/ai/movement/maneuvers/StrafeTargetManeuverV2", "$asmPath/features/maneuver/Strafe"),
+            newTransform("$vanillaPath/combat/ai/movement/maneuvers/B", "$asmPath/features/maneuver/Approach"),
+            newTransform("$vanillaPath/combat/ai/movement/maneuvers/U", "$asmPath/features/maneuver/Move"),
+        ))
+
+        val transformer2 = CCT(listOf(
+            CCT.newTransform("$vanillaPath/combat/ai/BasicShipAI", "$asmPath/asm/combat/ai/AssemblyShipAI"),
+            CCT.newTransform("$vanillaPath/combat/ai/I", "$asmPath/asm/combat/ai/OrderResponseModule"),
+
+            CCT.newTransform("$vanillaPath/combat/ai/movement/BasicEngineAI", "$asmPath/features/maneuver/OverrideEngineAI"),
+            CCT.newTransform("$vanillaPath/combat/ai/movement/maneuvers/StrafeTargetManeuverV2", "$asmPath/features/maneuver/Strafe"),
+            CCT.newTransform("$vanillaPath/combat/ai/movement/maneuvers/B", "$asmPath/features/maneuver/Approach"),
+            CCT.newTransform("$vanillaPath/combat/ai/movement/maneuvers/U", "$asmPath/features/maneuver/Move"),
+        ))
+
+        val stream = cl.getResourceAsStream("com/fs/starfarer/combat/ai/BasicShipAI.class")!!
+
+        var size = 0
+        var buffer = ByteArray(1024)
+        while (stream.available() > 0) {
+            size += stream.read(buffer, size, buffer.size - size)
+            if (size == buffer.size) {
+                buffer += ByteArray(buffer.size)
+            }
+        }
+
+
+        val vanillaClassData = buffer.sliceArray(IntRange(0, size - 1))
+
+        val c1 = transformer1.apply(vanillaClassData)
+        val c2 = transformer2.apply(vanillaClassData)
+
+        println(c1.size)
+        println(c2.size)
+
+
+
+//        stream.readBytes()
+
+//        transformer.apply(basicShipAIStream.readBytes())
+
+
+        //find $asm_root -type f -exec sed -i "s^$vanilla_path/BasicShipAI^$asm_path/AssemblyShipAI^g" {} +
+        //find $asm_root -type f -exec sed -i "s^$vanilla_path/I^$asm_path/OrderResponseModule^g" {} +
+        //
+        //# Override Engine Controller
+        //find $asm_root -type f -exec sed -i "s^$vanilla_path/movement/BasicEngineAI^$aitweaks_path/OverrideEngineAI^g" {} +
+        //
+        //# Override Maneuvers
+        //find $asm_root -type f -exec sed -i "s^$vanilla_path/movement/maneuvers/StrafeTargetManeuverV2^$aitweaks_path/Strafe^g" {} +
+        //find $asm_root -type f -exec sed -i "s^$vanilla_path/movement/maneuvers/void^$aitweaks_path/Intercept^g" {} +
+        //find $asm_root -type f -exec sed -i "s^$vanilla_path/movement/maneuvers/B^$aitweaks_path/Approach^g" {} +
+        //find $asm_root -type f -exec sed -i "s^$vanilla_path/movement/maneuvers/U^$aitweaks_path/Move^g" {} +
+
+
+//        val basicShipAI = cl.loadClass("com.fs.starfarer.combat.ai.BasicShipAI")
+
 
 //        print(basicShipAIStream.readBytes().size)
 
-        println(basicShipAI.isInstance(null))
+//        println(basicShipAI.isInstance(null))
 
     }
 }
