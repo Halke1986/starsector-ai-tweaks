c00332163f895c4d40ea7a341d025b882d37b22c
genir <mateusz.mikolaj.wojcik@gmail.com>
2024-11-10 15:18:40 +0100

    aim assist manager

--- src/com/genir/aitweaks/core/features/shipai/BaseShipAIPlugin.kt
+++ src/com/genir/aitweaks/core/features/shipai/BaseShipAIPlugin.kt
@@ -2,6 +2,7 @@ package com.genir.aitweaks.core.features.shipai
 
 import com.fs.starfarer.api.combat.ShipAIConfig
 import com.fs.starfarer.api.combat.ShipAIPlugin
+import com.fs.starfarer.api.combat.ShipwideAIFlags
 
 abstract class BaseShipAIPlugin : ShipAIPlugin {
     override fun setDoNotFireDelay(amount: Float) = Unit
@@ -11,6 +12,8 @@ abstract class BaseShipAIPlugin : ShipAIPlugin {
     override fun needsRefit(): Boolean = false
 
     override fun cancelCurrentManeuver() = Unit
+
+    override fun getAIFlags(): ShipwideAIFlags = ShipwideAIFlags()
 
     override fun getConfig(): ShipAIConfig = ShipAIConfig()
 }
