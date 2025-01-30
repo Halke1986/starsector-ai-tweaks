0ee2b5384c53c84a355baa21eb5390ffcff107f7
genir <mateusz.mikolaj.wojcik@gmail.com>
2025-01-30 22:40:54 +0100

    heap dump

--- launcher/src/com/genir/aitweaks/launcher/AITweaksEveryFrameScript.kt
+++ launcher/src/com/genir/aitweaks/launcher/AITweaksEveryFrameScript.kt
@@ -5,25 +5,36 @@ import com.fs.starfarer.api.Global
 import com.genir.aitweaks.launcher.loading.CoreLoaderManager
 import com.genir.aitweaks.launcher.loading.CoreLoaderManager.coreLoader
 import com.genir.aitweaks.launcher.loading.CoreLoaderManager.instantiate
+import com.sun.management.HotSpotDiagnosticMXBean
 import lunalib.lunaSettings.LunaSettings
 import org.lwjgl.input.Keyboard
+import java.lang.management.ManagementFactory
 
 class AITweaksEveryFrameScript : EveryFrameScript {
-    private var prevCoreScript: EveryFrameScript? = null
-    private var debounce = 0f
+    private var prevEfs: EveryFrameScript? = null
+    private var efsAge = 0f
 
     override fun isDone(): Boolean = false
 
     override fun runWhilePaused(): Boolean = true
 
     override fun advance(dt: Float) {
-        val devMode = LunaSettings.getBoolean("aitweaks", "aitweaks_enable_devmode") == true
-        if (devMode && Keyboard.isKeyDown(Keyboard.KEY_APOSTROPHE) && debounce <= 0f) {
-            CoreLoaderManager.updateLoader()
-            debounce = 1f
+        if (LunaSettings.getBoolean("aitweaks", "aitweaks_enable_devmode") != true) {
+            return
         }
 
-        debounce -= dt
+        efsAge += dt
+        if (Keyboard.isKeyDown(Keyboard.KEY_LBRACKET) && efsAge <= 1f) {
+            reloadEFS()
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_RBRACKET)) {
+            dumpHeap()
+        }
+    }
+
+    private fun reloadEFS() {
+        CoreLoaderManager.updateLoader()
 
         val efsClass: Class<*> = coreLoader.loadClass("com.genir.aitweaks.core.EveryFrameScript")
         val sector = Global.getSector()
@@ -31,10 +42,17 @@ class AITweaksEveryFrameScript : EveryFrameScript {
             return
         }
 
-        prevCoreScript?.let { sector.removeTransientScript(it) }
+        prevEfs?.let { sector.removeTransientScript(it) }
 
         val newScript: EveryFrameScript = efsClass.instantiate()
         sector.addTransientScript(newScript)
-        prevCoreScript = newScript
+        prevEfs = newScript
+    }
+
+    @Throws(Exception::class)
+    private fun dumpHeap() {
+        val mxBean = ManagementFactory.newPlatformMXBeanProxy(ManagementFactory.getPlatformMBeanServer(), "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean::class.java)
+        val path = "heapdump_${System.currentTimeMillis() / 1000}.hprof"
+        mxBean.dumpHeap(path, false)
     }
 }
