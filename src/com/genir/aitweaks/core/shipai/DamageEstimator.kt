39cc6fbebce0c3efce4ebc61ab6e185e7c99c3c8
genir <genir@noreply.com>
2025-05-15 22:39:58 +0200

    DamageEstimator

---
+++ src/com/genir/aitweaks/core/shipai/DamageEstimator.kt
@@ -0,0 +1,154 @@
+package com.genir.aitweaks.core.shipai
+
+import com.fs.starfarer.api.Global
+import com.fs.starfarer.api.combat.*
+import com.fs.starfarer.api.loading.MissileSpecAPI
+import com.genir.aitweaks.core.extensions.*
+import com.genir.aitweaks.core.utils.distanceToOrigin
+import com.genir.aitweaks.core.utils.sqrt
+import com.genir.aitweaks.core.utils.types.Direction.Companion.direction
+import kotlin.math.max
+
+class DamageEstimator(private val ship: ShipAPI) {
+    data class Damage(val finisherMissileDanger: Boolean, val damage: Float)
+
+    fun potentialDamage(duration: Float): Damage {
+        val dangerousWeapons = findDangerousWeapons(duration)
+
+        return Damage(
+            finisherMissileDanger = dangerousWeapons.any { weapon ->
+                weapon.isFinisherMissile
+            },
+
+            damage = dangerousWeapons.sumOf { weapon ->
+                potentialDamage(duration, weapon)
+            }
+        )
+    }
+
+    private fun findDangerousWeapons(duration: Float): List<WeaponAPI> {
+        val enemies: MutableList<ShipAPI> = mutableListOf()
+        val obstacles: MutableList<ShipAPI> = mutableListOf()
+
+        Global.getCombatEngine().ships.asSequence().forEach { entity ->
+            when {
+                entity.root == ship.root -> Unit
+                entity.isFighter -> Unit
+
+                entity.isHostile(ship) -> {
+                    // Don't consider overloaded or venting enemies.
+                    val tracker = entity.fluxTracker
+                    when {
+                        !tracker.isOverloadedOrVenting -> enemies.add(entity)
+                        duration - 2f > max(tracker.timeToVent, tracker.overloadTimeRemaining) -> enemies.add(entity)
+                    }
+
+                    // Consider slow enemies as weapon obstacles.
+                    if (!entity.isFast) obstacles.add(entity)
+                }
+
+                else -> if (!entity.isFast) obstacles.add(entity)
+            }
+        }
+
+        return enemies.flatMap { it.allWeapons }.filter { weapon ->
+            when {
+                weapon.isDisabled -> false
+                weapon.isPermanentlyDisabled -> false
+
+                // Non-weapons, such as decoratives.
+                weapon.derivedStats.dps == 0f -> false
+
+                // Assume the ship can recognize an empty
+                // missile launcher, same as the player can.
+                weapon.isMissile && weapon.isPermanentlyOutOfAmmo -> false
+
+                !canWeaponHitShip(duration, weapon, obstacles) -> false
+
+                else -> true
+            }
+        }
+    }
+
+    private fun canWeaponHitShip(duration: Float, weapon: WeaponAPI, obstacles: List<ShipAPI>): Boolean {
+        val toShip = ship.location - weapon.location
+        val distSqr = toShip.lengthSquared
+        val dist = sqrt(distSqr) - ship.shieldRadiusEvenIfNoShield / 2
+
+        // Check if the ship is out of weapons range.
+        val adjustedMovementTime = if (weapon.ship.engineController.isFlamedOut) 0f else duration
+        if (dist - adjustedMovementTime * weapon.ship.maxSpeed > weapon.totalRange) {
+            return false
+        }
+
+        // Check if the ship is out of weapons arc.
+        val isGuidedFinisherMissile = weapon.isFinisherMissile && !weapon.isUnguidedMissile
+        val weaponArc = weapon.absoluteArc.extendedBy(adjustedMovementTime * weapon.ship.maxTurnRate)
+        if (!isGuidedFinisherMissile && !weaponArc.contains(toShip.facing)) {
+            return false
+        }
+
+        // Check if projectile will reach the ship during venting.
+        val adjustedVentTime = duration - weapon.cooldownRemaining
+        if (dist / weapon.maxProjectileSpeed > adjustedVentTime) {
+            return false
+        }
+
+        // Check if the weapon is busy firing at another target.
+        if ((weapon.currAngle.direction - toShip.facing).length > 30f && weapon.isInFiringCycle && !isGuidedFinisherMissile) {
+            return false
+        }
+
+        return obstacles.none { obstacle ->
+            val p = weapon.location - obstacle.location
+
+            when {
+                // Obstacle does not block its own weapons.
+                obstacle.root == weapon.ship.root -> return@none false
+
+                // Weapon can shoot over allies.
+                weapon.noFF && obstacle.owner == weapon.ship.owner -> return@none false
+
+                // Special case for frigates. Assume they can quickly
+                // maneuver around an obstacle and deliver a torpedo.
+                weapon.ship.root.isFrigate && weapon.isFinisherMissile -> return@none false
+
+                // Obstacle is farther than the ship.
+                p.lengthSquared > distSqr -> return@none false
+            }
+
+            val radius = if (obstacle.isAlive) obstacle.shieldRadiusEvenIfNoShield * 1.5f
+            else obstacle.boundsRadius
+
+            distanceToOrigin(p, toShip) <= radius
+        }
+    }
+
+    private fun potentialDamage(duration: Float, weapon: WeaponAPI): Float {
+        val adjustedVentTime = duration - weapon.cooldownRemaining
+        val rawDamage = max(weapon.derivedStats.dps * adjustedVentTime, weapon.derivedStats.burstDamage)
+
+        val damageMultiplier = when {
+            weapon.isFinisherMissile -> 8f
+
+            weapon.damageType == DamageType.HIGH_EXPLOSIVE -> 2f
+
+            weapon.damageType == DamageType.FRAGMENTATION -> 0.5f
+
+            else -> 1f
+        }
+
+        return rawDamage * damageMultiplier
+    }
+
+    private val WeaponAPI.isFinisherMissile: Boolean
+        get() = isMissile && damageType == DamageType.HIGH_EXPLOSIVE
+
+    private val WeaponAPI.maxProjectileSpeed: Float
+        get() {
+            val missileSpec: MissileSpecAPI = spec.projectileSpec as? MissileSpecAPI ?: return projectileSpeed
+            val engineSpec: ShipHullSpecAPI.EngineSpecAPI = missileSpec.hullSpec.engineSpec
+            val maxSpeedStat = MutableStat(engineSpec.maxSpeed)
+            return maxSpeedStat.modifiedValue
+        }
+}
--- src/com/genir/aitweaks/core/shipai/VentModule.kt
+++ src/com/genir/aitweaks/core/shipai/VentModule.kt
@@ -1,20 +1,14 @@
 package com.genir.aitweaks.core.shipai
 
 import com.fs.starfarer.api.Global
-import com.fs.starfarer.api.combat.*
-import com.fs.starfarer.api.combat.DamageType.FRAGMENTATION
-import com.fs.starfarer.api.combat.DamageType.HIGH_EXPLOSIVE
-import com.fs.starfarer.api.loading.MissileSpecAPI
+import com.fs.starfarer.api.combat.ShipAPI
+import com.fs.starfarer.api.combat.ShipCommand
 import com.fs.starfarer.api.util.IntervalUtil
 import com.genir.aitweaks.core.extensions.*
 import com.genir.aitweaks.core.shipai.autofire.firingCycle
 import com.genir.aitweaks.core.shipai.movement.EngineController.Destination
 import com.genir.aitweaks.core.utils.defaultAIInterval
-import com.genir.aitweaks.core.utils.distanceToOrigin
-import com.genir.aitweaks.core.utils.sqrt
-import com.genir.aitweaks.core.utils.types.Direction.Companion.direction
 import org.lwjgl.util.vector.Vector2f
-import kotlin.math.max
 import kotlin.math.min
 import com.genir.aitweaks.core.shipai.Preset as AIPreset
 
@@ -33,7 +27,6 @@ class VentModule(private val ai: CustomShipAI) {
 
     private companion object Preset {
         const val ventTimeFactor = 0.8f
-        const val shipSpeedFactor = 0.75f
         const val idleVentThreshold = 0.30f
         const val opportunisticVentThreshold = 0.5f
 
@@ -218,139 +211,23 @@ class VentModule(private val ai: CustomShipAI) {
             return false
         }
 
-        val dangerousWeapons = findDangerousWeapons()
+        val estimator = DamageEstimator(ship)
+        val (finisherMissileDanger, potentialDamage) = estimator.potentialDamage(ventTime * ventTimeFactor)
         val effectiveHP: Float = ship.hitpoints * ship.hullLevel.let { it * it * it * it }
 
         return when {
-            dangerousWeapons.isEmpty() -> true
-
             // Don't get hit by a finisher missile.
-            dangerousWeapons.any { it.isFinisherMissile } -> false
+            finisherMissileDanger -> false
 
             // Received negligible damage.
             damageTracker.damage / effectiveHP < 0.03f -> true
 
             // Attempt to tank a limited amount of damage. 0.1f may seem like a large fraction,
             // but potential damage calculation is the absolute worst case scenario.
-            dangerousWeapons.sumOf { potentialDamage(it) } / effectiveHP < 0.1f -> true
+            potentialDamage / effectiveHP < 0.1f -> true
 
             else -> false
         }
-    }
-
-    private fun findDangerousWeapons(): List<WeaponAPI> {
-        val enemies: MutableList<ShipAPI> = mutableListOf()
-        val obstacles: MutableList<ShipAPI> = mutableListOf()
-
-        Global.getCombatEngine().ships.asSequence().forEach { entity ->
-            when {
-                entity.root == ship.root -> Unit
-                entity.isFighter -> Unit
-
-                entity.isHostile(ship) -> {
-                    // Don't consider overloaded or venting enemies.
-                    val tracker = entity.fluxTracker
-                    when {
-                        !tracker.isOverloadedOrVenting -> enemies.add(entity)
-                        ventTime - 2f > max(tracker.timeToVent, tracker.overloadTimeRemaining) -> enemies.add(entity)
-                    }
-
-                    // Consider slow enemies as weapon obstacles.
-                    if (!entity.isFast) obstacles.add(entity)
-                }
-
-                else -> if (!entity.isFast) obstacles.add(entity)
-            }
-        }
-
-        return enemies.flatMap { it.allWeapons }.filter { weapon ->
-            when {
-                weapon.isDisabled -> false
-                weapon.isPermanentlyDisabled -> false
-
-                // Non-weapons, such as decoratives.
-                weapon.derivedStats.dps == 0f -> false
-
-                // Assume the ship can recognize an empty
-                // missile launcher, same as the player can.
-                weapon.isMissile && weapon.isPermanentlyOutOfAmmo -> false
-
-                !canWeaponHitShip(weapon, obstacles) -> false
-
-                else -> true
-            }
-        }
-    }
-
-    private fun canWeaponHitShip(weapon: WeaponAPI, obstacles: List<ShipAPI>): Boolean {
-        val toShip = ship.location - weapon.location
-        val distSqr = toShip.lengthSquared
-        val dist = sqrt(distSqr) - ship.shieldRadiusEvenIfNoShield / 2
-
-        // Check if the ship is out of weapons range.
-        val adjustedMovementTime = if (weapon.ship.engineController.isFlamedOut) 0f else ventTime * shipSpeedFactor
-        if (dist - adjustedMovementTime * weapon.ship.maxSpeed > weapon.totalRange) {
-            return false
-        }
-
-        // Check if the ship is out of weapons arc.
-        val isGuidedFinisherMissile = weapon.isFinisherMissile && !weapon.isUnguidedMissile
-        val weaponArc = weapon.absoluteArc.extendedBy(adjustedMovementTime * weapon.ship.maxTurnRate)
-        if (!isGuidedFinisherMissile && !weaponArc.contains(toShip.facing)) {
-            return false
-        }
-
-        // Check if projectile will reach the ship during venting.
-        val adjustedVentTime = ventTime * ventTimeFactor - weapon.cooldownRemaining
-        if (dist / weapon.maxProjectileSpeed > adjustedVentTime) {
-            return false
-        }
-
-        // Check if the weapon is busy firing at another target.
-        if ((weapon.currAngle.direction - toShip.facing).length > 30f && weapon.isInFiringCycle && !isGuidedFinisherMissile) {
-            return false
-        }
-
-        return obstacles.none { obstacle ->
-            val p = weapon.location - obstacle.location
-
-            when {
-                // Obstacle does not block its own weapons.
-                obstacle.root == weapon.ship.root -> return@none false
-
-                // Weapon can shoot over allies.
-                weapon.noFF && obstacle.owner == weapon.ship.owner -> return@none false
-
-                // Special case for frigates. Assume they can quickly
-                // maneuver around an obstacle and deliver a torpedo.
-                weapon.ship.root.isFrigate && weapon.isFinisherMissile -> return@none false
-
-                // Obstacle is farther than the ship.
-                p.lengthSquared > distSqr -> return@none false
-            }
-
-            val radius = if (obstacle.isAlive) obstacle.shieldRadiusEvenIfNoShield * 1.5f
-            else obstacle.boundsRadius
-
-            distanceToOrigin(p, toShip) <= radius
-        }
-    }
-
-    private fun potentialDamage(weapon: WeaponAPI): Float {
-        val adjustedVentTime = ventTime * ventTimeFactor - weapon.cooldownRemaining
-        val rawDamage = max(weapon.derivedStats.dps * adjustedVentTime, weapon.derivedStats.burstDamage)
-
-        val damageMultiplier = when {
-            weapon.isFinisherMissile -> 4f
-
-            weapon.damageType == HIGH_EXPLOSIVE -> 2f
-
-            weapon.damageType == FRAGMENTATION -> 0.5f
-
-            else -> 1f
-        }
-
-        return rawDamage * damageMultiplier
     }
 
     /** Determine if ship should forego venting and backing off
@@ -414,15 +291,4 @@ class VentModule(private val ai: CustomShipAI) {
 
         return true
     }
-
-    private val WeaponAPI.isFinisherMissile: Boolean
-        get() = isMissile && damageType == HIGH_EXPLOSIVE
-
-    private val WeaponAPI.maxProjectileSpeed: Float
-        get() {
-            val missileSpec: MissileSpecAPI = spec.projectileSpec as? MissileSpecAPI ?: return projectileSpeed
-            val engineSpec: ShipHullSpecAPI.EngineSpecAPI = missileSpec.hullSpec.engineSpec
-            val maxSpeedStat = MutableStat(engineSpec.maxSpeed)
-            return maxSpeedStat.modifiedValue
-        }
 }
